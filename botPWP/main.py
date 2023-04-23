7ddfcb7323872878001061fc51d1be731ac79987
Wellinston <wellinston.silva@gmail.com>
2023-04-23 20:40:38 -0300

    Bot naruto

---
+++ botPWP/main.py
@@ -0,0 +1,17 @@
+import discord
+
+class MyClient(discord.Client):
+  async def on_ready(self):
+    print("logged on as {0}!" .format(self.user))
+
+  async def on_message(self, message):
+    print("Message from {0.author}: {0.content}" .format(message))
+    if message.content == "!ola":
+      await message.channel.send(f"Olá {message.author.name}! Prazer em conhece-lo")
+    if message.content == "!imagem":
+      await message.channel.send(file=discord.File("Images/sasuke.jpeg"))
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run("MTA5OTgwOTU3MjkxMjM4MjA2Mg.GPbDUb.6gPLJmAiHAlDGwAks1qjFpvi6lhedTWm62TOw8")
